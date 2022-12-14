f9250a647a4c35bd687e3bd050c5f7bbba620a0c
alekzandro <58974374+alekzandro@users.noreply.github.com>
2022-12-14 14:06:50 +0100

    Update EmailClient.java

--- EmailClient.java
+++ EmailClient.java
@@ -1,78 +1,117 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Base64;
 import java.util.Scanner;
-
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 
-
-public class EmailClient {
-    private static String host ="smtp.kth.se";
+public class SendEmailClient {
+    private static String host = "smtp.kth.se";
     private static int port = 587;
+    private static char[] buffer = new char[10000];
+    private static String command;
+    private static int charsRead = -1;
+    private static SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+    private static Scanner scanner = new Scanner(System.in);
+    private static final String USERNAME_BASE64 = "VXNlcm5hbWU6";
+    private static final String PASSWORD_BASE64 = "UGFzc3dvcmQ6";
 
-    public static void main(String[] args) throws UnknownHostException, IOException{
-       // SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-       // SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket(host, port);
-       Socket socket = new Socket(host, port);
-       PrintWriter writer = new PrintWriter(socket.getOutputStream());
-     BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-       try {
-       
-        
+    public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
         String line="";
-        String command ="";
-        char[] buffer = new char[10000];
-        int charsRead=-1;
-        Scanner scanner = new Scanner(System.in);
+        Socket socket = new Socket(host, port);
+        PrintWriter writer = new PrintWriter(socket.getOutputStream());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+        // Setup: starting TLS connection
+
+        System.out.println("\n\n-- Starting SMTP -- \n\n\tTo exit, type q\n\n");
+        System.out.println(readFromBuffer(reader));
+        toServer(writer, "ehlo smtp.kth.se");
+        System.out.println(readFromBuffer(reader));
+
+        toServer(writer, "starttls");
+        if ((line = readFromBuffer(reader)).contains("Ready to start TLS")) {
+            socket = (SSLSocket) sslSocketFactory.createSocket(socket, host, port, true);
+            writer = new PrintWriter(socket.getOutputStream());
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        }
+        System.out.println(line);
+        toServer(writer, "ehlo smtp.kth.se");
+        System.out.println(readFromBuffer(reader));
+
+        // TSL started
+        writeFromCommandLine(writer, false);
         do {
-            while((charsRead = reader.read(buffer)) != -1){
-                if (charsRead < 1000) {
-                    line += String.valueOf(buffer).substring(0, charsRead);
-                    break;
-                } else {   
-                    line += String.valueOf(buffer);
-                }
-            }
+            line = readFromBuffer(reader);
             System.out.println(line);
-            line = "";
-            command = scanner.nextLine();
-            System.out.println("Sending to server: " + command);
-            writer.println(command);
-            writer.flush();
+            if(line.contains(USERNAME_BASE64) || line.contains(PASSWORD_BASE64)){
+                writeFromCommandLine(writer, true);
+            } else if (line.contains("354 End data with")){
+                scanner.useDelimiter("\r\n.\r\n");
+                String emailBody ="";
+                emailBody+= scanner.next();
+                scanner.reset();
+                //Consume the . character
+                scanner.nextLine();
+                scanner.nextLine();  
+                System.out.println("Sending to server: " + emailBody);
+                emailBody += "\r\n.\r\n";
+                writer.print(emailBody);
+                writer.flush();
+            } else {
+                writeFromCommandLine(writer, false);
+            }
             
-        } while(!command.equals("exit"));
 
-       } catch (SocketException e){
-        socket = new Socket(host, port);
-        writer = new PrintWriter(socket.getOutputStream());
-        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        
-        e.printStackTrace();
-       }
-       
-
-
-
-       
-        
-
-        
-        
-       
-
-        
-
-
-
-       
+        } while (!command.equals("q"));
     }
-    
 
+    private static String readFromBuffer(BufferedReader reader) throws IOException {
+        String line="";
+        while ((charsRead = reader.read(buffer)) != -1) {
+            if (charsRead < 1000) {
+                line += String.valueOf(buffer).substring(0, charsRead);
+                break;
+            } else {
+                line += String.valueOf(buffer);
+            }
+        }
+        return line;
+    }
+
+    private static void toServer(PrintWriter writer, String msg) {
+        writer.print(msg + "\r\n");
+        writer.flush();
+        System.out.println(msg + "\n");
+    }
+
+    private static void writeFromCommandLine(PrintWriter writer, boolean encode) {
+        command = scanner.nextLine();
+        if (isQ(command)) {
+            quit();
+        }
+        if(encode){
+            toServer(writer, encodeToBase64(command));
+        } else {
+            toServer(writer, command);
+        }
+        
+    }
+
+    private static boolean isQ(String s) {
+        return s.equals("q");
+    }
+
+    private static void quit() {
+        System.out.println("Exiting program");
+        System.exit(0);
+    }
+
+    private static String encodeToBase64(String string){
+        return Base64.getEncoder().encodeToString(string.getBytes());
+    }
 }
